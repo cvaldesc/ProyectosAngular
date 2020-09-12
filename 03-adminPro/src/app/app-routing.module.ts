d57d028a91c690aeff4d49b770033899536c6e1e
cvaldesc <cvaldsavello@gmail.com>
2020-09-12 00:43:00 -0300

    rutas listas

--- 03-adminPro/src/app/app-routing.module.ts
+++ 03-adminPro/src/app/app-routing.module.ts
@@ -1,34 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoudComponent } from './pages/nopagefoud/nopagefoud.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+
+import { PageRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoudComponent } from './nopagefoud/nopagefoud.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-    ],
-  },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
   { path: '**', component: NopagefoudComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), PageRoutingModule, AuthRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
