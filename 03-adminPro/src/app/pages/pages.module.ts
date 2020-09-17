fdc3ae2ad960eac27195c17874d93d7a590729b0
cvaldesc <cvaldsavello@gmail.com>
2020-09-17 17:37:48 -0300

    pasar datos por input y output

--- 03-adminPro/src/app/pages/pages.module.ts
+++ 03-adminPro/src/app/pages/pages.module.ts
@@ -8,26 +8,29 @@ import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from '../components/components.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent,
-  ],
-  exports: [
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent,
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    SharedModule,
-    RouterModule,
-    ComponentsModule,
-  ],
+	declarations: [
+		DashboardComponent,
+		ProgressComponent,
+		Grafica1Component,
+		PagesComponent,
+		AccountSettingsComponent,
+	],
+	exports: [
+		DashboardComponent,
+		ProgressComponent,
+		Grafica1Component,
+		PagesComponent,
+		AccountSettingsComponent,
+	],
+	imports: [
+		CommonModule,
+		FormsModule,
+		SharedModule,
+		RouterModule,
+		ComponentsModule,
+	],
 })
-export class PagesModule {}
+export class PagesModule { }
